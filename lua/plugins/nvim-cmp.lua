80bdb646dfbb31bbbc7a89daf49e8dccdc0887b8
hanlen <hanlen@meta-boot-6.localdomain>
2025-01-22 10:33:07 +0800

    update for nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,44 +1,55 @@
 return {
-    -- 代码补全插件
     "hrsh7th/nvim-cmp",
     dependencies = {
-        "hrsh7th/cmp-nvim-lsp",  -- LSP 源
-        "hrsh7th/cmp-buffer",    -- 缓冲区补全
-        "hrsh7th/cmp-path",      -- 路径补全
-        "hrsh7th/cmp-cmdline",   -- 命令行补全
-        "L3MON4D3/LuaSnip",      -- Snippet 引擎
-        "saadparwaiz1/cmp_luasnip", -- Snippet 源
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
     },
     config = function()
         local cmp = require("cmp")
+        local luasnip = require("luasnip")
+
         cmp.setup({
-            -- 指定 snippet 引擎
-            experimental = {
-                ghost_text = false, --隐藏编辑时默认补全的内容,否则影响可读性 
-            },
             snippet = {
                 expand = function(args)
-                -- For `vsnip` users.
-                vim.fn["vsnip#anonymous"](args.body)
+                    luasnip.lsp_expand(args.body) -- Use LuaSnip for snippet expansion
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                    ['<tab>'] = cmp.mapping(function(fallback)
-                        -- Hint: if the completion menu is visible select next one
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }), -- i - insert mode; s - select mode
-                    ['<CR>'] = cmp.mapping.confirm { select = true },
-                }),
-            -- 补全源
+                -- Use <Tab> to navigate completion menu and jump between snippet placeholders
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item() -- Navigate completion menu
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump() -- Jump to next snippet placeholder
+                    else
+                        fallback() -- Fallback to default <Tab> behavior
+                    end
+                end, { "i", "s" }), -- i - insert mode; s - select mode
+
+                -- Use <S-Tab> to navigate backward in completion menu and snippet placeholders
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item() -- Navigate backward in completion menu
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1) -- Jump to previous snippet placeholder
+                    else
+                        fallback() -- Fallback to default <S-Tab> behavior
+                    end
+                end, { "i", "s" }),
+
+                -- Confirm completion
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
-                -- For vsnip users.
-                { name = "vsnip" },
-            }, { { name = "buffer" }, { name = "path" } }),
+                { name = "luasnip" }, -- Use LuaSnip for snippets
+                { name = "buffer" },
+                { name = "path" },
+            }),
         })
 
         -- / 查找模式使用 buffer 源
@@ -46,17 +57,17 @@ return {
             mapping = cmp.mapping.preset.cmdline(),
             sources = {
                 {
-                    name = "buffer", 
+                    name = "buffer",
                     option = {
                         get_bufnrs = function()
                             return vim.api.nvim_list_bufs()
-                        end
-                    }
-                }
+                        end,
+                    },
+                },
             },
         })
 
-        -- : 命令行模式中使用 path 和 cmdline 源.
+        -- : 命令行模式中使用 path 和 cmdline 源
         cmp.setup.cmdline(":", {
             mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources({
